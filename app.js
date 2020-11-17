969bf2d4aa78c7af94cb95daf2e8775715a8714d
Angelica <angelicagolphin@yahoo.com>
2020-11-17 15:36:22 -0500

    finished adding code for hex.js to create the random hex generator

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ btn.addEventListener("click", function () {
 
     //get random number between 0-3 (array syntax)
     const randomNumber = getRandomNumber();
-    console.log(randomNumber);
+    //console.log(randomNumber);
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
